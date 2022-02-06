d3930d57fad56d1ed5b59705f3fd7eada74049d9
Andy Wang <andyjw2@illinois.edu>
2022-02-06 17:41:00 -0600

    Created spaceship json

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // Wishing you good luck on your Adventure!
+        System.out.println("Welcome aboard the Rocinante!");
+        // use scanner
     }
 }
